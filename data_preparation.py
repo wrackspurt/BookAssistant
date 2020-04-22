0129cc503f27369d02b71c0275ab655d480553d1
Lisa <fanciful.mischief@gmail.com>
2020-04-22 21:42:30 +0300

    added data

--- data_preparation.py
+++ data_preparation.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 
 def get_data_from_csv():
-    books = pd.read_csv('books.csv')
+    books = pd.read_csv('data/books.csv')
     books.replace(to_replace='J.K. Rowling, Mary GrandPré', value='J.K. Rowling', inplace=True)
-    ratings, to_read, tags_1, tags_2 = pd.read_csv('ratings.csv'), pd.read_csv('to_read.csv'),\
-                                       pd.read_csv('book_tags.csv'), pd.read_csv('tags.csv')
+    ratings, to_read, tags_1, tags_2 = pd.read_csv('data/ratings.csv'), pd.read_csv('data/to_read.csv'), \
+                                       pd.read_csv('data/book_tags.csv'), pd.read_csv('data/tags.csv')
     tags = pd.merge(tags_1, tags_2, left_on='tag_id', right_on='tag_id', how='inner')
     return books, ratings, to_read, tags
 
